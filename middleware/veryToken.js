ba9a176b7265f28f958d842292b030b74d41a553
norealy <xdatgd@gmail.com>
2020-12-28 09:56:28 -0500

    Refresh uid and token-id

--- middleware/veryToken.js
+++ middleware/veryToken.js
@@ -6,7 +6,7 @@ const secretAccessKey = process.env.ACCESS_TOKEN_KEY || 'RW5jb2RlIHRvIEJhc2U2NCB
 const duration = parseInt(process.env.JWT_DURATION || 2400);
 
 module.exports.verifyAccessToken = function (req,res,next){
-    const accessToken = req.header('access-token');
+    const accessToken = req.header('Access_Token');
     console.log(accessToken)
     if(!accessToken) return res.status(401).send('Access Denis !')
     try {
@@ -18,51 +18,52 @@ module.exports.verifyAccessToken = function (req,res,next){
 }
 
 module.exports.verifyRefreshToken = async function (req,res,next){
-    const refreshToken = req.header('refresh-token');
+    const refreshToken = req.header('Refresh_Token');
     if(!refreshToken) return res.status(401).send('Refresh Denis !')
     try {
-        const accessToken = req.header('access-token');
+        const accessToken = req.header('Access_Token');
         const signature = accessToken.split('.')[2]
         console.log("signature : ",signature)
         const alg = "HS256";
         const verified = await jws.verify(accessToken,alg,secretAccessKey);
         console.log("verified : ",verified);
-        // console.log(tokenSuccess);
         if(verified){
             const jwsData = jws.decode(accessToken)
-            console.log("UID : ",jwsData.payload['uid'])
+            const uid = jwsData.payload['uid']
+            console.log("UID : ",uid)
             let token_id = decryption.decryptToken(refreshToken)
             console.log("TOKEN_ID : ",token_id)
-            const tokenSuccess = await Tokens.findOne({"uid_token":token_id}) // {"uid_token":token_id,"user_uid":jwsData.payload['uid']}
-            console.log(tokenSuccess)
+            // const tokenSuccess = await Tokens.findOne({"user_uid":uid}) // {"uid_token":token_id , "user_uid":uid}
+            const tokenSuccess = await Tokens.findOne({"user_uid":uid,"uid_token":token_id}) 
+            console.log("tokenSuccess:",tokenSuccess)
             if(tokenSuccess){
                 const iat = Math.floor(new Date()/1000);
                 const exp = iat + duration;
                 const access_Token =  jws.sign({
                     header: {alg:'HS256',typ:'JWT'},
-                    payload: {uid: jwsData.payload['uid'], iat, exp},
+                    payload: {uid: uid, iat, exp},
                     secret:secretAccessKey
                 });
                 return res.status(200).send( {New_AccsessToken:access_Token} );
             }
-            return res.status(400).send( "Access Token Denis ! !" );
+            return res.status(400).send( "Token Denis ! !" );
         }
         return res.status(400).send("Access Token Denis ! !" );
     } catch (error) {
-        console.log(error)
+        // console.log(error)
         return res.status(400).send('Invalid Token');
     }
 }
 // {
 //     "user": {
-//         "_id": "5fe9e976f5c3232e17fcbd77",
-//         "username": "usernamet1",
-//         "password": "$2b$10$8xzjkMO7zDUt/YCuhq/4K.JvJ1/Fo8sLW1K7FbgBqDXmviGV6rgAW",
+//         "_id": "5fe9f08ba3d822183d34d4a9",
+//         "username": "usernamet4",
+//         "password": "$2b$10$6McSpnUWgrv085dwdv5czeV.pWvtdyCfQeV8aHvIL9cDKSTCJchzy",
 //         "email": "xdatgd223@gmail.com",
 //         "phone": "0332302626",
 //         "__v": 0
 //     },
-//     "Access_Token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmU5ZTk3NmY1YzMyMzJlMTdmY2JkNzciLCJpYXQiOjE2MDkxNjUxNzQsImV4cCI6MTYwOTE2ODc3NH0.ZvFnfYGTQpWdeE0_GqA3QaCuV2zVK9gscLAdEh5WumM",
-//     "Refresh_Token": "842759f1dfd6eaa3f096a5fc28bebe4ff34bd7f3c438ec0adf805830180b04ba2d954d9a",
-//     "uid_token": "036d989c-abf6-44aa-9308-0c9d9dca4396"
+//     "Access_Token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmU5ZjA4YmEzZDgyMjE4M2QzNGQ0YTkiLCJpYXQiOjE2MDkxNjY5ODcsImV4cCI6MTYwOTE3MDU4N30.H4guxMe6w9ZcBV6Ya13GBkw5PX7Bs2mfOVxrUXH0ZQg",
+//     "Refresh_Token": "d2225fa0d4dceaf4f0c7f0f82dbebe19a71bd7f2916cb70a8d805831420904eb2c96429a",
+//     "uid_token": "f6052294-07b3-4b51-8fdc-bc9ecfc05066"
 // }
